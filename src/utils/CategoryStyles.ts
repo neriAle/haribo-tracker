a3068110520009c2824c776ed484378024dc9821
Alessandro Neri <a.neri@student.tudelft.nl>
2026-07-06 17:00:43 +0200

    💄 Changed category colors

--- src/utils/CategoryStyles.ts
+++ src/utils/CategoryStyles.ts
@@ -6,36 +6,31 @@ export type CategoryStyle = {
 export const getCategoryStyle = (category: string): CategoryStyle => {
   const styles: Record<string, CategoryStyle> = {
     Gommose: {
-      style: "background-color: var(--brand-green); color: #022c22;",
-    },
-    Frizzanti: {
       style:
         "background-color: var(--brand-yellow); color: color-mix(in srgb, var(--brand-pink) 60%, black); ",
     },
+    Frizzanti: {
+      class: "bg-teal-100 text-teal-800",
+    },
     Mix: {
       style:
-        "background-color:var(--brand-blue); color: white;",
+        "background: linear-gradient(to right, var(--brand-pink), var(--brand-blue), var(--brand-green)); color: white;",
     },
     Marshmallow: {
       class: "bg-white text-black border border-neutral-200",
     },
     Natale: {
-      style:
-        "background-color: var(--brand-red); " +
-        "color: black; " +
-        "background-image: radial-gradient(circle, rgba(255,255,255,0.9) 1px, transparent 1px), radial-gradient(circle, rgba(255,255,255,0.9) 1px, transparent 1px); " +
-        "background-size: 8px 8px; " +
-        "background-position: 0 0, 4px 4px;",
+      style: "background-color: var(--brand-red); color: white; ",
     },
     "Limited Edition": {
-      style:
-        "background: linear-gradient(to right, var(--brand-pink), var(--brand-blue), var(--brand-green)); color: white;",
+      style: "background-color:var(--brand-pink); color: white;",
     },
     Liquirizie: {
       class: "bg-neutral-800 text-white",
     },
     Pasqua: {
-      class: "bg-teal-100 text-teal-800",
+      style:
+        "background-color: color-mix(in srgb, var(--brand-green) 60%, white); color: #022c22;",
     },
   };
 
